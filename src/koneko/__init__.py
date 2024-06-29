45a50dbd88d35c8c81eb5d9bd83c2ba24b4ccfcf
Rychanya <rychanya@gmail.com>
2024-06-29 09:59:41 +0300

    💩

--- src/koneko/__init__.py
+++ src/koneko/__init__.py
@@ -1,11 +1,21 @@
-from asyncio import Event, sleep
+from asyncio import (
+    Queue,
+    TimeoutError,
+    wait_for,
+)
 from collections.abc import AsyncIterable
+from datetime import datetime
 
-from blacksheep import Application, Request, Response, get, html
+from blacksheep import Application, Request, Response, get, html, post
+from blacksheep.server.openapi.v3 import OpenAPIHandler
 from blacksheep.server.process import is_stopping
 from blacksheep.server.sse import ServerSentEvent, ServerSentEventsResponse
+from openapidocs.v3 import Info  # type: ignore
 
 app = Application()
+docs = OpenAPIHandler(info=Info(title="Example API", version="0.0.1"))
+docs.bind_app(app)
+queue: Queue[ServerSentEvent] = Queue()
 
 
 @get("/")
@@ -32,20 +42,30 @@ async def index() -> Response:
     return html(value=res)
 
 
-async def stop_checker(request: Request, stop_event: Event):
-    while True:
-        if is_stopping():
-            stop_event.set()
-        if await request.is_disconnected():
-            stop_event.set()
-        await sleep(1)
+class SSEWrapper:
+    def __init__(self, request: Request, queue: Queue[ServerSentEvent], timeout: int = 1) -> None:
+        self._request = request
+        self.queue = queue
+        self.timeout = timeout
 
+    async def is_connected(self) -> bool:
+        return not (is_stopping() or await self._request.is_disconnected())
 
-async def sse_generator() -> AsyncIterable[ServerSentEvent]:
-    await sleep(0)
-    yield ServerSentEvent(data="1")
+    async def generator(self) -> AsyncIterable[ServerSentEvent]:
+        while await self.is_connected():
+            try:
+                yield await wait_for(self.queue.get(), self.timeout)
+            except TimeoutError:
+                yield ServerSentEvent(data=f"{datetime.now()}", event="ping")
 
 
 @get("/events")
 async def events(request: Request):
-    return ServerSentEventsResponse(events_provider=sse_generator)
+    wrapper = SSEWrapper(request=request, queue=queue)
+    return ServerSentEventsResponse(events_provider=wrapper.generator)
+
+
+@post("/")
+async def create_event():
+    await queue.put(ServerSentEvent(data=f"event at {datetime.now()}"))
+    return "ok"
